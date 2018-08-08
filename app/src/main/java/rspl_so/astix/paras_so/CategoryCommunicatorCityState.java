25fa5cca26826ea0d8b8d7336b3ad8a1ae8deb5f
Yogender <astixset2@gmail.com>
2018-08-08 13:49:27 +0530

    initial commit

---
+++ app/src/main/java/rspl_so/astix/paras_so/CategoryCommunicatorCityState.java
@@ -0,0 +1,10 @@
+package rspl_so.astix.paras_so;
+
+import android.app.Dialog;
+
+public interface CategoryCommunicatorCityState {
+	
+	public void selectedCityState(String selectedCategory, Dialog dialog, int flgCityState);
+
+
+}
